cac75543fd4296a55753815f17bf114b740d84b1
Tejas Patel <your-email@example.com>
2026-07-16 07:48:22 -0400

    Describe what you changed

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 
+from dashboard_engine import load_dashboard_data
+
 st.title("Test")
 
-st.success("Pandas imported successfully")
+st.success("dashboard_engine imported successfully")
